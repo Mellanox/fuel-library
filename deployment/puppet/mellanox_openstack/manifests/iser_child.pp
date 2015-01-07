e30dae5cc431c83fe286b199045bfe81cd91f96a
Aviram Bar-Haim <aviramb@mellanox.com>
2015-01-07 16:46:14 +0200

    Add support for untagged VLAN in storage network
    Change-Id: If25f2d38b367b20a0bfb488c85aa75ab698878ae
    Signed-off-by: Aviram Bar-Haim <aviramb@mellanox.com>

--- deployment/puppet/mellanox_openstack/manifests/iser_child.pp
+++ deployment/puppet/mellanox_openstack/manifests/iser_child.pp
@@ -4,25 +4,25 @@ class mellanox_openstack::iser_child ($storage_parent, $iser_pkey){
   $iser_script_dir = '/opt/iser'
   $iser_child_script = "$iser_script_dir/iser_child_create.sh"
 
-  file { $iser_script_dir:
-    ensure => directory,
-  }
+  if $iser_pkey {
+    file { $iser_script_dir:
+      ensure => directory,
+    }
 
-  file { $iser_child_script:
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '500',
-    content => template('mellanox_openstack/iser_child_create.erb'),
-  }
+    file { $iser_child_script:
+      ensure  => file,
+      owner   => 'root',
+      group   => 'root',
+      mode    => '500',
+      content => template('mellanox_openstack/iser_child_create.erb'),
+    }
 
-  exec { 'iser_child_create':
-    command   => "bash $iser_child_script",
-    unless    => "test -f $interfaces_path/$storage_parent",
-    path      => ['/usr/bin','/usr/sbin','/bin','/sbin','/usr/local/bin'],
-    logoutput => true,
-    require   => File[$iser_child_script],
+    exec { 'iser_child_create':
+      command   => "bash $iser_child_script",
+      path      => ['/usr/bin','/usr/sbin','/bin','/sbin','/usr/local/bin'],
+      logoutput => true,
+      require   => File[$iser_child_script],
+    }
   }
-
 }
 
